292db2b061dd23dbb14c714a7753f76ed062a5de
RenzoSC <renzoc009@gmail.com>
2024-06-22 15:49:08 -0300

    hot fixes

--- Backend/db_com/customers.py
+++ Backend/db_com/customers.py
@@ -1,5 +1,5 @@
 from models.customer_m import Customer
-from config.database import customer_collection
+from config.database import customer_collection as db
 from schema.customer import customerEntity, customersEntity
 from bson import ObjectId
 from pymongo.errors import WTimeoutError, PyMongoError
@@ -8,9 +8,9 @@ class customerComunicationDB():
 
     def create_customer(customer: Customer):
         try:
-            result = customer_collection.insert_one(dict(customer))
-            customer_created = customer_collection.find_one({"_id":result.inserted_id})
-            return customerEntity(customer_created)
+            result = db.insert_one(dict(customer))
+            customer_created = db.find_one({"_id":result.inserted_id})
+            return customer_created
         except WTimeoutError:
             return {"error":"Timeout expired... query error"}
         except PyMongoError as e:
@@ -18,19 +18,16 @@ class customerComunicationDB():
     
     def get_customer_by_id(id:str):
         try:
-            customer = customer_collection.find_one({"_id":ObjectId(id)})
-            if customer:
-                return customerEntity(customer)
-            else:
-                return {"error":"Customer not found!"}
+            customer = db.find_one({"_id":ObjectId(id)})
+            return customer
         except WTimeoutError:
             return {"error":"Timeout expired... query error"}
         except PyMongoError as e:
             return {"error":str(e)}
     
-    def get_list_of_customers():
+    def get_list_of_customer():
         try:
-            customers = customersEntity(customer_collection.find())
+            customers = db.find()
             return customers
         except WTimeoutError:
             return {"error":"Timeout expired... query error"}
@@ -39,11 +36,8 @@ class customerComunicationDB():
     
     def update_customer_by_id(id:str, customer:Customer):    
         try:
-            result = customer_collection.find_one_and_update({"_id":ObjectId(id)},{"$set":dict(customer)}, return_document=True)
-            if result:
-                return {"success": "Updated customer successfully"}
-            else:
-                return {"error": "Customer not found"}
+            result = db.find_one_and_update({"_id":ObjectId(id)},{"$set":dict(customer)}, return_document=True)
+            return result
         except WTimeoutError:
             return {"error":"Timeout expired... query error"}
         except PyMongoError as e:
@@ -51,11 +45,8 @@ class customerComunicationDB():
     
     def delete_customer_by_id(id:str):
         try:
-            result = customer_collection.find_one_and_delete({"_id":ObjectId(id)})
-            if result:
-                return {"success": "Deleted customer successfully"}
-            else:
-                return {"error": "Customer not found"}
+            db.find_one_and_delete({"_id":ObjectId(id)})
+            return {"success": "Deleted customer successfully"}
         except WTimeoutError:
             return {"error":"Timeout expired... query error"}
         except PyMongoError as e:
--- Backend/routes/customers.py
+++ Backend/routes/customers.py
@@ -1,30 +1,56 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from typing import List
 from models.customer_m import Customer
 from db_com.customers import customerComunicationDB
+from schema.customer import customerEntity, customersEntity
 
 #customers /clientes
 #iniciar el server : uvicorn customers:app --reload
 router = APIRouter()
 
 @router.get("/customers", response_model=List[dict]) 
-async def get_all_customers():
-    return customerComunicationDB.get_list_of_customers()
+async def get_all_customer():
+    try:
+        customers = customerComunicationDB.get_list_of_customer()
+        return customersEntity(customers)
+    except:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Server error")
 
 @router.post("/customers")
 async def create_customer(customer : Customer):
     #falta validación de datos acá ?¿
-    return customerComunicationDB.create_customer(customer)
-
+    try:
+        customer_created = customerComunicationDB.create_customer(customer)
+        return customerEntity(customer_created)
+    except Exception as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Server error")
+    
+    
 @router.get("/customers/{id}")
 async def get_customer(id:str):
-    return customerComunicationDB.get_customer_by_id(id)
+    try:
+        customer = customerComunicationDB.get_customer_by_id(id)
+        if not customer:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Customer not found")
+        return customerEntity(customer)
+    except:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Server error")
 
 @router.put("/customers/{id}")
 async def update_customer(id:str, customer:Customer):
-    #falta validación de datos acá ?¿
-    return customerComunicationDB.update_customer_by_id(id, customer)
-
+    try:
+        result =customerComunicationDB.update_customer_by_id(id, customer)
+        if not result:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Customer not found")
+        return {"success": "Updated customer successfully"} 
+    except:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Server error")
+    
 @router.delete("/customers/{id}")
 async def delete_customer(id:str):
-    return customerComunicationDB.delete_customer_by_id(id)
+    try: 
+        if not customerComunicationDB.get_customer_by_id(id):
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Customer not found")
+        return customerComunicationDB.delete_customer_by_id(id)
+    except:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Server error")
