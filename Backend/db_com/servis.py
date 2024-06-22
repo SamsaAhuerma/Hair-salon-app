33cac239ef7909090f589464c606ed9636443822
nicoxynos5 <nicoxynos@mi.unc.edu.ar>
2024-06-22 17:47:07 -0300

    fix(servis): naming fixes and exception handling

--- Backend/db_com/servis.py
+++ Backend/db_com/servis.py
@@ -1,25 +1,50 @@
-from config.database import servis_collection
-from pymongo import errors
+from config.database import db as db
+from pymongo.errors import DuplicateKeyError ,WTimeoutError, PyMongoError
 
 class ServisRepository:
 
     def create_servis(self, servis):
         try:
-            servis_collection.insert_one(dict(servis))
-        except errors.DuplicateKeyError:
+            db.insert_one(dict(servis))
+        except DuplicateKeyError:
             raise Exception("Servis already exists")
+        except WTimeoutError:
+            raise Exception("Timeout expired querying the database")
+        except PyMongoError as e:
+            raise Exception(f"An error occurred while getting the servis list {e}")
 
     def servis_exists(self, servis_name):
-        return servis_collection.find_one({"servis_name": servis_name})
+        return db.find_one({"servis_name": servis_name})
     
-    def get_all_servis(self):
-        return servis_collection.find()
-    
-    def get_servis_by_id(self, servis_id):
-        return servis_collection.find_one({"_id": servis_id})
-    
-    def update_servis(self, servis_id, servis):
-        servis_collection.find_one_and_update({"_id": servis_id}, {"$set": dict(servis)})
+    def get_list_of_servis(self):
+        try:
+            return db.find()
+        except WTimeoutError:
+            raise Exception("Timeout expired querying the database")
+        except PyMongoError as e:
+            raise Exception(f"An error occurred while getting the servis list {e}")
 
-    def delete_servis(self, servis_id):
-        servis_collection.find_one_and_delete({"_id": servis_id})
+    def get_servis_by_id(self, servis_id):
+        try:
+            return db.find_one({"_id": servis_id})
+        except WTimeoutError:
+            raise Exception("Timeout expired querying the database")
+        except PyMongoError as e:
+            raise Exception(f"An error occurred while getting the servis list {e}")
+    
+    def update_servis_by_id(self, servis_id, servis):
+        try:
+            return db.find_one_and_update({"_id": servis_id}, {"$set": dict(servis)})
+        except WTimeoutError:
+            raise Exception("Timeout expired querying the database")
+        except PyMongoError as e:
+            raise Exception(f"An error occurred while getting the servis list {e}")
+
+    def delete_servis_by_id(self, servis_id):
+        try:
+            return db.find_one_and_delete({"_id": servis_id})
+        except WTimeoutError:
+            raise Exception("Timeout expired querying the database")
+        except PyMongoError as e:
+            raise Exception(f"An error occurred while getting the servis list {e}")
+        
