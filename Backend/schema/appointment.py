cd4dabd490a5a94891bf17c18bc3d8bcfba4e575
Samsa <samsaahuerma@gmail.com>
2024-06-22 15:27:57 -0300

    schema de appointment

---
+++ Backend/schema/appointment.py
@@ -0,0 +1,12 @@
+def appointmentEntity(item) -> dict:
+    return {
+        "id": str(item["_id"]),
+        "customer_id": item["customer_id"],
+        "service_id": item["service_id"],
+        "time": item["time"],
+        "date": item["date"],
+        "message": item["message"]
+    }
+
+def appointmentsEntity(appointments) -> list:
+    return [appointmentEntity(appointment) for appointment in appointments]
