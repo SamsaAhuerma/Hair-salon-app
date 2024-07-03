45ab9ec7fc591918e39f884b53d39b2ed52dc24a
Samsa <samsaahuerma@gmail.com>
2024-07-03 13:44:43 -0300

    Correccion en routes

--- Backend/routes/appointments.py
+++ Backend/routes/appointments.py
@@ -2,33 +2,33 @@ from fastapi import APIRouter, HTTPException, status
 from typing import List
 from models.appointments_m import Appointments
 from schema.appointment import appointmentEntity, appointmentsEntity
-from db_com.appointments import  appointmentsComunicationDB as db
+from db_com.appointments import  appointmentsComunicationDB 
 from bson import ObjectId
 
 router = APIRouter(prefix="/appointments",
                    tags=["appointments"],
                    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"description": "Not found"}})
 
-com_db = db()
+com_db = appointmentsComunicationDB()
 #citas
 @router.get("/", response_model=List[Appointments]) 
 async def get_all_appointments():
     try: 
-        return db.get_list_of_appointments()
+        return com_db.get_list_of_appointments()
     except Exception as e:
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR0, detail="No se pudieron obtener las citas" + str(e))
 
 @router.post("/", response_model=Appointments, status_code=201)
 async def create_appointements(appointment: Appointments):
     try: 
-        return db.create_appointment(appointment) 
+        return com_db.create_appointment(appointment) 
     except Exception as e:
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="No se pudo crear la cita" + str(e))
     
 @router.get("/{id}")
 async def get_appointment(id: str):
     try:
-        appointment = db.get_appointment_by_id(id) #a implementar
+        appointment = com_db.get_appointment_by_id(id) 
         if appointment is None:
             raise HTTPException(status_code=404, detail=f"No se encontró una cita con el ID: {id}")
         return appointmentEntity(appointment)
@@ -48,7 +48,7 @@ async def update_appointment(id: str, appointment_update_data: Appointments):
 @router.delete("/{id}", response_model=Appointments)
 async def delete_appointment(id: str):
     try:
-        db.delete_appointment_by_id(id) 
+        com_db.delete_appointment_by_id(id) 
         return {"message": "Cita eliminada con éxito"}
     except HTTPException as http_exc:
         raise http_exc
